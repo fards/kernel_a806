b6a7f1031a47a12afb39ff1843857ba49b9a5b60
Adrian Ulrich <adrian@blinkenlights.ch>
2013-09-22 20:00:20 +0200

    Add splashgenerator to buildscript

--- _build.sh
+++ _build.sh
@@ -5,7 +5,12 @@ make ARCH=arm clean
 make ARCH=arm CROSS_COMPILE=$TCHAIN fusion3_yuga_defconfig
 make ARCH=arm CROSS_COMPILE=$TCHAIN -j 8
 
+# Create new bootsplash
+( cd _create_bootsplash && make clean ; make )
+
+# add new kernel
 cp arch/arm/boot/zImage ../../../device/sony/c6603/kernel 
+# copy new bootsplash to device tree
+cp _create_bootsplash/logo.rle ../../../device/sony/c6603/rootdir/logo.rle
 # overwrite wlan driver of sony
 cp ./drivers/staging/prima/wlan.ko  ../../../vendor/sony/c6603/proprietary/lib/modules/wlan.ko
-
