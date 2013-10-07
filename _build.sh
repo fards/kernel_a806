8ca674aae8bf37e848135dc8294f76aa0e399d95
fards <fardsmobile@yahoo.com>
2013-10-07 11:32:54 +0100

    add in Voltage sysfs from Faux and drop the voltage lower limit.

--- _build.sh
+++ _build.sh
@@ -1,16 +1,19 @@
 
 export TCHAIN=../../../prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
 
-make ARCH=arm clean
+#make ARCH=arm clean
 make ARCH=arm CROSS_COMPILE=$TCHAIN fusion3_yuga_defconfig
-make ARCH=arm CROSS_COMPILE=$TCHAIN -j 8
+make ARCH=arm CROSS_COMPILE=$TCHAIN -j 4
 
 # Create new bootsplash
 ( cd _create_bootsplash && make clean ; make )
 
 # add new kernel
-cp arch/arm/boot/zImage ../../../device/sony/c6603/kernel 
+cp arch/arm/boot/zImage ../../boot
 # copy new bootsplash to device tree
-cp _create_bootsplash/logo.rle ../../../device/sony/c6603/rootdir/logo.rle
-# overwrite wlan driver of sony
-cp ./drivers/staging/prima/wlan.ko  ../../../vendor/sony/c6603/proprietary/lib/modules/wlan.ko
+cp _create_bootsplash/logo.rle ../../logo.rle
+# have somewhere easy to copy the wlan module to. 
+cp ./drivers/staging/prima/wlan.ko  ../../vendor/sony
+# Run mkbootimg for the terminally lazy
+cd ../../boot/
+./makeit.sh
